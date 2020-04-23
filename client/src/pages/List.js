7a5b7da74d167e9b2a7a4d7f2454d29237d493cf
Calvin Macintosh <calvinmac633@gmail.com>
2020-04-22 21:37:27 -0400

    routes working

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../components/Grid";
-// import { List, ListItem } from "../components/List";
-// import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Lists() {
     // Setting our component's initial state
