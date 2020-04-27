66f13b417009f76c2ccb7c844378b05fde6daf15
Calvin Macintosh <calvinmac633@gmail.com>
2020-04-27 17:12:27 -0400

    cleaned up pages and components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Table } from 'react-bootstrap';
 import API from "../utils/API"
-import Additem from "../components/Additem"
-import { List } from "../components/List"
 import Carousel from "../components/Carousel";
 
 
--- client/src/pages/ListPage.js
+++ client/src/pages/ListPage.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Table } from 'react-bootstrap';
 import API from "../utils/API"
-import Additem from "../components/Additem"
-import { List } from "../components/List"
 
 export function ListPage() {
     const [list, setList] = useState([]);
