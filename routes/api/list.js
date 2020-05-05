dfd996d8fba3c4ef04f0727fbb65972986a735b4
Jacob Scarf Merrell <jacobscarfmerrell@gmail.com>
2020-05-04 20:48:08 -0400

    feat: add createList endpt

--- routes/api/list.js
+++ routes/api/list.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const listController = require("../../controllers/listController");
 
 // Matches with "/api/list"
+
+router.route('/:listname')
+  .post(listController.create)
+
 router
   .route("/:codename")
   .get(listController.findOne)
-  .post(listController.create)
   .put(listController.findOneAndUpdate);
 
 module.exports = router;
